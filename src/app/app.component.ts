ef9ea0b8ba5c98c4d585db209a7b30bb66099e42
adamdrm <adamdermawan535@gmail.com>
2024-10-18 23:03:12 +0700

    build task service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {UserComponent} from './user/user.component';
+import {DUMMY_USERS} from "./api/dummy-users";
+import {TasksComponent} from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent ],
 })
 export class AppComponent {
-  title = 'first-angular-app';
+ users = DUMMY_USERS;
+ selectedUserId?: string;
+
+  get selectedUser() {
+    console.log(this.users.find(x => x.id === this.selectedUserId))
+   return this.users.find(user => user.id === this.selectedUserId);
+
+  }
+
+ onSelectUser(id : string){
+  this.selectedUserId = id;
+ }
+
+
 }
+
+
